423e9eda7ff69a8a83a5cbe7ec24813343ff2769
lproko <laurenproko@gmail.com>
2024-12-27 22:43:27 +0200

    feat(fe): activate contact form

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -3,57 +3,48 @@ import { Grid } from "@mui/material";
 import { Container, Row, Col } from "react-bootstrap";
 import "animate.css";
 import contactImg from "../../assets/contact-img.svg";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import TrackVisibility from "react-on-screen";
 import "./Contact.scss";
 import Footer from "../footer/Footer";
 import { useTranslation } from "react-i18next";
+import emailjs from "emailjs-com";
 
 function Contact() {
-  const formInitialDetails = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    message: "",
-  };
-  const { t } = useTranslation();
-  const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState(t("Contact.Form.Send"));
-  const [status, setStatus] = useState({
-    success: false,
-    message: "",
-  });
+  const form = useRef<HTMLFormElement>(null);
 
-  const onFormUpdate = (category: string, value: string) => {
-    setFormDetails({
-      ...formDetails,
-      [category]: value,
-    });
-  };
-
-  const handleSubmit = async (e: any) => {
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setButtonText(t("Contact.Form.Sending"));
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    setFormDetails(formInitialDetails);
-    if (result.code == 200) {
-      setStatus({ success: true, message: "Message sent successfully" });
-    } else {
-      setStatus({
-        success: false,
-        message: "Something went wrong, please try again later.",
-      });
+    if (!form.current) {
+      console.error("Form reference is null");
+      return;
     }
+
+    setButtonText(t("Contact.Form.Sending"));
+    setDisabled(true);
+    emailjs
+      .sendForm(
+        "service_k3okjir",
+        "template_qmp0ir2",
+        form.current,
+        "XmBGVxR1lVmj3jd2D"
+      )
+      .then(
+        () => {
+          alert("Your Message send it successfully");
+          (e.target as HTMLFormElement).reset();
+          setButtonText(t("Contact.Form.Send"));
+          setDisabled(false);
+        },
+        (error) => {
+          alert("FAILED..." + error.text);
+        }
+      );
   };
+
+  const { t } = useTranslation();
+  const [buttonText, setButtonText] = useState(t("Contact.Form.Send"));
+  const [disabled, setDisabled] = useState(false);
 
   return (
     <Grid className="contact" id="Contact">
@@ -81,72 +72,46 @@ function Contact() {
                   }
                 >
                   <h2 style={{ color: "gray" }}>{t("Contact.Contact")}</h2>
-                  <form onSubmit={handleSubmit}>
+                  <form ref={form} onSubmit={sendEmail}>
                     <Row>
                       <Col size={12} sm={6} className="px-1">
                         <input
+                          name="firstName"
                           type="text"
-                          value={formDetails.firstName}
                           placeholder={t("Contact.Form.FirstName")}
-                          onChange={(e) =>
-                            onFormUpdate("firstName", e.target.value)
-                          }
                         />
                       </Col>
                       <Col size={12} sm={6} className="px-1">
                         <input
+                          name="lastName"
                           type="text"
-                          value={formDetails.lastName}
                           placeholder={t("Contact.Form.LastName")}
-                          onChange={(e) =>
-                            onFormUpdate("lastName", e.target.value)
-                          }
                         />
                       </Col>
                       <Col size={12} sm={6} className="px-1">
                         <input
                           type="email"
-                          value={formDetails.email}
+                          name="email"
                           placeholder={t("Contact.Form.Email")}
-                          onChange={(e) =>
-                            onFormUpdate("email", e.target.value)
-                          }
                         />
                       </Col>
                       <Col size={12} sm={6} className="px-1">
                         <input
                           type="tel"
-                          value={formDetails.phone}
+                          name="phone"
                           placeholder={t("Contact.Form.Phone")}
-                          onChange={(e) =>
-                            onFormUpdate("phone", e.target.value)
-                          }
                         />
                       </Col>
                       <Col size={12} className="px-1">
                         <textarea
                           rows={6}
-                          value={formDetails.message}
+                          name="message"
                           placeholder={t("Contact.Form.Message")}
-                          onChange={(e) =>
-                            onFormUpdate("message", e.target.value)
-                          }
                         ></textarea>
-                        <button type="submit">
+                        <button type="submit" disabled={disabled}>
                           <span>{buttonText}</span>
                         </button>
                       </Col>
-                      {status.message && (
-                        <Col>
-                          <p
-                            className={
-                              status.success === false ? "danger" : "success"
-                            }
-                          >
-                            {status.message}
-                          </p>
-                        </Col>
-                      )}
                     </Row>
                   </form>
                 </div>
